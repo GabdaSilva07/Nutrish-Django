7af0dc2d7ed2a6835c29661039e32f8bb80a3903
Gabriel da Silva <gabriel.dasilva2017@my.ntu.ac.uk>
2021-08-21 10:33:00 +0100

    Loging and register working

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('auth/register/', RegisterAPI.as_view()),
     path('auth/signin/', LoginAPI.as_view()),
     path('auth/user/', UserAPI.as_view()),
+    path('auth/logout/', knox_views.LogoutView.as_view(),'knox_logout'),
 ]
