57fc4ac3b0c622cff757fb5cfbdc3917767871cf
Gabriel da Silva <gabriel.dasilva2017@my.ntu.ac.uk>
2021-08-21 12:42:28 +0100

    Updated 21/08

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -3,12 +3,9 @@ from django.http import JsonResponse
 from django.urls import path, include
 
 
-def home(request):
-    return JsonResponse({"hello": "world!"})
-
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     path("api/", include('UserAPI.urls')),
     path("", include('accounts.urls')),
 ]
