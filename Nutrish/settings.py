16b0963f72ccca375e310816318c31ed14231446
Gabriel da Silva <gabriel.dasilva2017@my.ntu.ac.uk>
2021-08-22 18:55:30 +0100

    Debug updated

--- Nutrish/settings.py
+++ Nutrish/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'please-change-me')
-DEBUG = True
-# os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
