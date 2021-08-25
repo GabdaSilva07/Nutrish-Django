b020c8224a26fd935fa2ba1657dd99b3f1e9058a
Gabriel da Silva <gabriel.dasilva2017@my.ntu.ac.uk>
2021-08-25 13:58:47 +0100

    Create user and Login is working

--- Nutrish/urls.py
+++ Nutrish/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('UserInfo.urls', namespace='UserInfo_API')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest-framework')),
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest-framework')),    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
